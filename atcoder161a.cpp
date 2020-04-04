419e5e46bc8a26e72e6fbbc5947882e5659417f4
AshrafulMorsalinSizan99 <50795768+AshrafulMorsalinSizan99@users.noreply.github.com>
2020-04-04 18:28:58 +0600

    Add files via upload

---
+++ atcoder161a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[3];
+    cin>>a[0]>>a[1]>>a[2];
+    swap(a[0],a[1]);
+    swap(a[0],a[2]);
+    cout<<a[0]<<" "<<a[1]<<" "<<a[2];
+    return 0;
+}
